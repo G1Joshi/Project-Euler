f1ddc881766e2815ca22aa0b71a7f5d0c8b1038d
Jeevan Chandra Joshi <52780516+G1Joshi@users.noreply.github.com>
2020-09-28 16:58:48 +0530

    25164150

--- Problem Set/Problem 6.py
+++ Problem Set/Problem 6.py
@@ -1,14 +1,14 @@
-def sqofsm(n):
+def sqrofsum(n):
     sum=0
     for i in range(n+1):
         sum+=i
     return sum*sum
 
-def smofsq(n):
+def sumofsqr(n):
     sum=0
     for i in range(n+1):
         sum+=i*i
     return sum
 
 n=100
-print(sqofsm(n)-smofsq(n))
+print(sqrofsum(n)-sumofsqr(n))
